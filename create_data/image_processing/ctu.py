10104077abb802cdafce501cfc42eaa886f3a34c
langfeng <langfeng.cs@gmail.com>
2025-08-14 22:36:53 +0800

    Add Copyright

--- create_data/image_processing/ctu.py
+++ create_data/image_processing/ctu.py
@@ -1,5 +1,4 @@
-# Copyright 2025 Zhejiang University (ZJU), China.
-# Copyright 2025 TimeMaster team.
+# Copyright 2025 Zhejiang University (ZJU), China and TimeMaster Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
